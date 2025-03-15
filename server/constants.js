557ce542b750d30a2dc7972865d4984f2cf41311
A01723229 <A01723229@tec.mx>
2025-03-15 12:10:41 -0600

    Home dashboard, procedures etc...

--- server/constants.js
+++ server/constants.js
@@ -7,16 +7,16 @@ const dbName = "CRM_DB";
 const serverPort = 3000;
 const api = "/api";
 
-// Queries
-const getTotalSales = "SELECT COUNT(*) AS totalSales FROM Deal";
-const getTotalIncome = "SELECT SUM(DealValue) AS totalIncome FROM Deal";
-const getTotalCustomers = "SELECT COUNT(*) AS totalCustomers FROM Customer";
-const getCompletionRate = "SELECT (COUNT(CASE WHEN Status = 'Completed' THEN 1 END) / COUNT(*)) * 100 AS completionRate FROM Task";
-const getTaskStats = "SELECT Priority, COUNT(*) AS count FROM Task GROUP BY Priority";
-const getCompletedTasks = "SELECT COUNT(*) AS completedTasks FROM Task WHERE Status = 'Completed'";
-const getOverdueTasks = "SELECT COUNT(*) AS overdueTasks FROM Task WHERE DeadLine < CURDATE() AND Status != 'Completed'";
-const getTasksCloseToDeadline = "SELECT Title, DeadLine FROM Task WHERE DeadLine BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 7 DAY)";
-const getPastDeals = "SELECT c.ClientName, d.DealValue, d.Comission, d.DeadLine, d.PaymentStatus FROM Deal d INNER JOIN Customer c ON d.CustomerID = c.CustomerID ORDER BY d.DeadLine DESC LIMIT 5";
+// Procedures
+const getTotalSales = "EXEC GetTotalSales";
+const getTotalIncome = "EXEC GetTotalIncome";
+const getTotalCustomers = "EXEC GetTotalCustomers";
+const getCompletionRate = "EXEC GetCompletionRate";
+const getTaskStats = "EXEC GetTaskStats";
+const getCompletedTasks = "EXEC GetCompletedTasks";
+const getOverdueTasks = "EXEC GetOverdueTasks";
+const getTasksCloseToDeadline = "EXEC GetTasksCloseToDeadline";
+const getPastDeals = "EXEC GetPastDeals";
 
 module.exports = {
    dbHost, dbPort, dbUser, dbPass, dbName, serverPort, api,
