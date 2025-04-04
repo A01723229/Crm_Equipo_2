21807327a9298387963f9e5be24199f924d3ec65
A01723229 <A01723229@tec.mx>
2025-04-04 08:15:24 -0600

    updated url

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     setError("");
 
     try {
-      const response = await fetch('http://localhost:3001/api/login', {
+      const response = await fetch('https://crm-equipo-2.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -22,7 +22,7 @@ export default function SignUp() {
     setError("");
 
     try {
-      const response = await fetch('http://localhost:3001/api/signup', {
+      const response = await fetch('https://crm-equipo-2.vercel.app/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password, role, company, sellerName }),
