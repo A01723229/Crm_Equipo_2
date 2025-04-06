38a9b5cb78816f7bf3311636473ca3612a58aba7
A01723229 <A01723229@tec.mx>
2025-04-06 12:45:00 -0600

    gemini

--- server/route.js
+++ server/route.js
@@ -3,7 +3,7 @@ const { getData } = require("./RestControllers/getData.js");
 const { postLogin } = require("./RestControllers/postLogin.js");
 const { postSignup } = require("./RestControllers/postSignup.js");
 const { getMe } = require("./RestControllers/getMe");
-const getGemini = require("./RestControllers/getGemini");
+const postGemini = require("./RestControllers/postGemini");
 const authMiddleware = require("./middleware/auth");
 const router = express.Router();
 
@@ -11,6 +11,6 @@ router.get("/data", getData);
 router.post("/login", postLogin);
 router.post("/signup", postSignup);
 router.get("/me", authMiddleware, getMe);
-router.post("/gemini", getGemini);
+router.post("/gemini", postGemini);
 
 module.exports = router;
