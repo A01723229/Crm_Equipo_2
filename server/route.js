9eb00f1d864b77931745061411b3bdefd20623fc
A01723229 <A01723229@tec.mx>
2025-04-06 10:53:11 -0600

    vercel

--- server/route.js
+++ server/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { getData } = require("./RestControllers/getData.js");
 const { postLogin } = require("./RestControllers/postLogin.js");
 const { postSignup } = require("./RestControllers/postSignup.js");
+const { getMe } = require("./RestControllers/getMe");
 const authMiddleware = require("./middleware/auth");
 const router = express.Router();
 
