a986a39c2f65b321c089b5d64892e28537f4bf0c
A01723229 <A01723229@tec.mx>
2025-04-25 07:56:34 -0600

    cambios getme

--- server/controllers/getMe.js
+++ server/controllers/getMe.js
@@ -1,12 +1,13 @@
 const getMe = (req, res) => {
   console.log("/me route hit");
   console.log("Authenticated user:", req.user);
-    res.json({
-      sellerName: req.user.SellerName,
-      email: req.user.email,
-      role: req.user.role,
-      company: req.user.company,
-    });
-  };
-  
-  module.exports = { getMe };
+
+  res.json({
+    sellerName: req.user.SellerName,
+    email: req.user.Email,
+    role: req.user.Role,
+    company: req.user.Company,
+  });
+};
+
+module.exports = { getMe };
