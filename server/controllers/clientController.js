bfcf11b48f0b41e99e07e2f882803495c71951fc
A01723229 <A01723229@tec.mx>
2025-04-28 12:18:35 -0600

    cambios

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -2,8 +2,17 @@ const { sql, poolPromise } = require("../database/db");
 
 exports.addClient = async (req, res) => {
   try {
-    const { ClientName, Company, Description, Telephone, Email, SellerID } = req.body;
-    
+    const { ClientName, Company, Description, Telephone, Email } = req.body;
+    const sellerId = req.user?.SellerID;
+
+    console.log("Decoded user:", req.user);
+    console.log("Adding client:", { ClientName, Company, Telephone, Email, SellerID: sellerId });
+
+    if (!sellerId) {
+      console.error("SellerID not found on req.user");
+      return res.status(400).json({ error: "SellerID not found in user token" });
+    }
+
     const pool = await poolPromise;
     await pool.request()
       .input("ClientName", sql.VarChar, ClientName)
@@ -11,15 +20,18 @@ exports.addClient = async (req, res) => {
       .input("Description", sql.VarChar, Description)
       .input("Telephone", sql.VarChar, Telephone)
       .input("Email", sql.VarChar, Email)
-      .input("SellerID", sql.Int, SellerID) 
+      .input("SellerID", sql.Int, sellerId)  
       .execute("AddClient");
-      
+
+    console.log("Client added successfully!");
+
     res.status(201).json({ message: "Client added" });
   } catch (err) {
     console.error("AddClient error:", err);
     res.status(500).json({ error: "Failed to add client" });
   }
 };
+
 
 exports.updateClient = async (req, res) => {
   try {
